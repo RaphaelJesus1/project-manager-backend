4945a3a1b415bd8a9b6a285338fda912cc30154d
Raphael Menezes <raphael-mj@hotmail.com>
2023-12-01 04:07:46 -0300

    feature(helper): adding httpHelper

---
+++ src/presentation/helpers/httpHelper.ts
@@ -0,0 +1,31 @@
+import { HttpResponse } from "../../application/protocols";
+import { ServerError, UnauthorizedError } from "../errors";
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+});
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+});
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack),
+});
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError(),
+});
